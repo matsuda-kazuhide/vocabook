8d8ece66fd06c5ccc9892d1b07929c6c23a6fecc
Yuto Matayoshi <e185742@ie.u-ryukyu.ac.jp>
2019-11-21 19:17:59 +0900

    2nd commit

---
+++ vocabook/vocabook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vocabook
+//
+//  Created by 又吉　雄斗 on 2019/11/21.
+//  Copyright © 2019 Matayoshi Yuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ vocabook/vocabookTests/vocabookTests.swift
@@ -0,0 +1,34 @@
+//
+//  vocabookTests.swift
+//  vocabookTests
+//
+//  Created by 又吉　雄斗 on 2019/11/21.
+//  Copyright © 2019 Matayoshi Yuto. All rights reserved.
+//
+
+import XCTest
+@testable import vocabook
+
+class vocabookTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
